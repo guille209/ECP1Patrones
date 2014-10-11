01e84b85035b0c1583b5f0d23372ac19e860330e
guille209 <gguillemiranda@gmail.com>
2014-10-11 20:16:19 +0200

    Añadido Main

---
+++ src/main/java/es/upm/miw/pd/vehiculos/main/VehiculosMain.java
@@ -0,0 +1,13 @@
+package es.upm.miw.pd.vehiculos.main;
+import upm.*;
+
+public class VehiculosMain {
+	
+	VehiculoController controller = new VehiculoController();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
